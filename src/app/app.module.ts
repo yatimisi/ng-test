7b7c3f5dce09e86dafeffed9fd4847c3bc33defb
Yatimisi <yatimisi2012@gmail.com>
2019-05-01 19:02:37 +0800

    2019/04/24 angular exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lesson1Component } from './lesson1/lesson1.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lesson1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lesson1/lesson1.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson1',
+  templateUrl: './lesson1.component.html',
+  styleUrls: ['./lesson1.component.css']
+})
+export class Lesson1Component implements OnInit {
+  name = 'Lux';
+  url = 'https://www.google.com';
+  money = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  sayHello() {
+    alert(this.name);
+  }
+
+}
